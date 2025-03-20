fe31f522258fe934e63ff225644cb9f3cfe2f325
DukeOmni <caioscript@gmail.com>
2025-03-20 16:15:47 -0300

    Modules and Project tree

---
+++ src/app/core/modules/home/components/punch-clock/punch-clock.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-punch-clock',
+  imports: [],
+  templateUrl: './punch-clock.component.html',
+  styleUrl: './punch-clock.component.css'
+})
+export class PunchClockComponent {
+
+}
---
+++ src/app/core/modules/home/components/time-clock/time-clock.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-time-clock',
+  imports: [],
+  templateUrl: './time-clock.component.html',
+  styleUrl: './time-clock.component.css'
+})
+export class TimeClockComponent {
+
+}
